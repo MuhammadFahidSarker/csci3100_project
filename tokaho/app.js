20e29f7182ea6b63d06c7cacc479dc181b96bbc8
tokaho <tokaho20001228@gmail.com>
2022-02-10 18:16:07 +0800

    remove key

--- tokaho/app.js
+++ tokaho/app.js
@@ -2,18 +2,7 @@
 
 
 // credential
-const serviceAccount = {
-  "type": "service_account",
-  "project_id": "test-96f35",
-  "private_key_id": "d80466738d96767bfcef7ad27d6b21b0cb1fa82b",
-  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDX0WO6nLx9gfFv\nEBOEIlTtgypW/tffIVM8WkgLbP6zKdNo2xjAtx4uPFhIo40ndbp9HYa4vIGvMLxy\nWcBqPQ9I/xkE7tr8quyu3bZargQG//QXIA7VZrI/aZqPsm8gmwGsJQ2Wt82036DG\nZ791LJ/zylSybaNF4lYtMHLmcH0y+zdSYl4dpG2GbR62sEgzu0+JuTxfLHY/L2HE\nPl2WntpBFnxK2ZOuhBkIWkRplXSBjj1HvncdNqTTfO1V1OtGJgzRvzqRlJpCnkPo\nsBnh0JH4bUfzB4JCUa207ZMLQ/oo2Xb4I9PV8VylcgWJ7HlstjwdvRCyevuV9XEp\nm69knLp5AgMBAAECggEAD0HPMCqzRu0oMrmQePjOB7d23Le/Q5fbxkpnSaApi7lz\nJ7hw0h5FR4VTxBjZ2lZf5MdGVlTZ7IHAb2tpvKj3OxTGVvCwMbNqBZJeI/V63+gk\nKvwEyHCDkpOQ426SHXGP3J9cS/wk/PqKkhQqDGrR86kUCUQnyiw40DuFV/ClDTTk\n1e/qrewBq6XG8yZI7ukl+Kr+E3s8ZwPr85903NAz9nYBfDIxgRBL75cOQ/injmmV\nuC8LJ3eJ8T6pntVFtDVhfURHplkgPiR37ikQ58AZQrqTFR1qp7ZdFKQJdlWeSh3D\nRfGKj/ugdwpmKxJyQz/VOC0/k9QprP5aB0FqnCVY7QKBgQD5xekEFkzENU88syNo\ndWtyafWx3uMWwMl/ij9AtMJSePXvjrafRxfiMsaexFmTzGHiNAR9JlgZlx+FdlZ8\nlc1YBM2+a+JmmD9wA//Ed9+D7OrEHu5Ew4GX5LTPxCJRUF8cFEnx6pwqTDZd1jtW\nuGBPtizQO7zqrKiUhHeBtK6kTQKBgQDdMsW5y0w/BWuqwQGGrPsA6DdjOcJ5kuz3\nDO/NrsLOz9nKLk5bAKVhQNFJgbDqQivKlIZO77eLK2L66SyK8YrfCzzGMgVmuqeA\njFT+bg//NsdgoBcPp4Z+H/wWXXkVjRDr1JHQ7VWkPz4/7jDy8byhR9EZAMiCS3Ht\nbDyOz/503QKBgQCkZh95ETR18KC20zH7D04pgJS2/n15/GqkB9qaPF+q6PL3Onf5\nXlHaEJGq7pIHNf83ZAWQWiyiq1WUAs/qMK93v7GsJFZgMNWQZRFzv3Le4pGjhLhP\nil9CGsbn+bCc1MnFMZV6JXGkZYVjPh7QdYru7HmSlb9Ct2I+XvJJ7SMGAQKBgD5D\n3ITARWY4EGawjc1rQ/ytYWn9CFFQmoOwkJBFcXDl8ViIUGYteGA37rHSYAqRNeKA\njI4LzI7QV7xphrBV7dF7SFBCJGMWh7AX0JUORlKBCP6aEePGNIWsWzmwdjUq0GiS\nHbnCFSl24/76/axlf8Omg6WWItsKgbdjkGhjBC3BAoGAF4WsUtqIM7NcYf2UNJ31\nupVTbf+erAJ3EQPw/pDp8oufCfQfqQCP5lF0+ANILTfu7EPNfGFIoPWAd6/LP8Uc\nbJY8Lw8q0O6qTNLeoPRtecF6+SZ03qvHfN1ne37XWNQxYyDbgETMjPZvP3n123f1\ntOu3emNBBImFudRJFTWWLls=\n-----END PRIVATE KEY-----\n",
-  "client_email": "firebase-adminsdk-m8zbg@test-96f35.iam.gserviceaccount.com",
-  "client_id": "114619107020117158943",
-  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-  "token_uri": "https://oauth2.googleapis.com/token",
-  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-m8zbg%40test-96f35.iam.gserviceaccount.com"
-}
+
 
 
 // Library
