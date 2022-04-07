0007f6174e148c354949566e442db5d8d8a1b306
adikkarol <adikkarol01@gmail.com>
2022-04-07 13:58:13 +0800

    added scanfile
    need to handle url from the front end

--- tokaho/app.js
+++ tokaho/app.js
@@ -13,26 +13,23 @@ uses:
 const serviceAccount = require('./SDK/test-96f35-firebase-adminsdk-m8zbg-8e10e14cd1.json')
 const functions = require('firebase-functions')
 const admin = require('firebase-admin')
-const express = require("express")
-const path = require("path")
-const cors = require("cors")
-const cookieParser = require("cookie-parser")
-const nodemailer = require("nodemailer");
-const bodyParser = require('body-parser');
-const xoauth2 = require('xoauth2');
-const {
-  Firestore
-} = require('@google-cloud/firestore');
+const express = require('express')
+const path = require('path')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+const nodemailer = require('nodemailer')
+const bodyParser = require('body-parser')
+const xoauth2 = require('xoauth2')
+const { Firestore } = require('@google-cloud/firestore')
 const os = require('os')
 // initialization
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  projectId: "test-96f35",
-  databaseURL: "https://test-96f35-default-rtdb.firebaseio.com"
+  projectId: 'test-96f35',
+  databaseURL: 'https://test-96f35-default-rtdb.firebaseio.com',
 })
 const app = express()
 const PORT = 8080
-
 
 /*
 custom functions
@@ -40,41 +37,42 @@ custom functions
 const central_auth = require('./centralAuth.js')
 const registration = require('./registration.js')
 const groupfunctions = require('./groupfunctions.js')
+const scanfile = require('./scanfile.js')
 //
-app.use(cookieParser());
-app.use(bodyParser());
+app.use(cookieParser())
+app.use(bodyParser())
 
 //DEBUG
 {
-app.use((req, res, next) => {
-  console.log('-----------------------')
-  console.log('Time: ', Date.now());
-  next();
-});
-app.use((req, res, next) => {
-  console.log('Request type: ', req.method);
-  console.log('request body')
-  console.log(req.body)
-  next();
-});
+  app.use((req, res, next) => {
+    console.log('-----------------------')
+    console.log('Time: ', Date.now())
+    next()
+  })
+  app.use((req, res, next) => {
+    console.log('Request type: ', req.method)
+    console.log('request body')
+    console.log(req.body)
+    next()
+  })
 }
 
 // not guarded by centralAuth
-app.use('/register', registration.authenticate);
-app.get('/verify', registration.verify_email);
+app.use('/register', registration.authenticate)
+app.get('/verify', registration.verify_email)
 
 //Gateway - centralAuth
-app.use('/apis', central_auth.central_auth);
+app.use('/apis', central_auth.central_auth)
 //guarded by centralAuth
-app.use('/apis/creategroup',groupfunctions.creategroup);
-app.use('/apis/deletegroup',groupfunctions.deletegroup);
-app.use('/apis/updategroup',groupfunctions.updategroup);
-app.use('/apis/querygroup',groupfunctions.querygroup);
-app.use('/apis/listgroup',groupfunctions.listgroup);
+app.use('/apis/creategroup', groupfunctions.creategroup)
+app.use('/apis/deletegroup', groupfunctions.deletegroup)
+app.use('/apis/updategroup', groupfunctions.updategroup)
+app.use('/apis/querygroup', groupfunctions.querygroup)
+app.use('/apis/listgroup', groupfunctions.listgroup)
+app.use('/scandocument', scanfile.scanfile)
 
 //DEBUG page
 app.use(express.static(path.join(__dirname, 'public')))
-
 
 // serve
 app.listen(PORT, () => console.log(`serving on port ${PORT}`))
---
+++ tokaho/scanfile.js
@@ -0,0 +1,71 @@
+// library
+const vision = require('@google-cloud/vision')
+// client
+const client = new vision.ImageAnnotatorClient({
+  // keyFilename in SDK
+  keyFilename: './SDK/test-96f35-firebase-adminsdk-m8zbg-8e10e14cd1.json',
+})
+
+class Assert extends Error {
+  constructor(message) {
+    super(message) // (1)
+    this.name = 'Assertion'
+    this.errormsg = message
+  }
+}
+
+// dummy function to check routes
+const checkread = async (req, res, next) => {
+  //console.log(`Check Read Passed`)
+  return res.status(200).json({ Success: `Check Read Passed` })
+}
+
+const scanfile = async (req, res, next) => {
+  if (!req.body.url) {
+    return res.status(401).json({
+      Error: 'File not found',
+    })
+  }
+  try {
+    // Need to change the default relative url to the req.body.url
+    const [result] = await client.documentTextDetection(
+      `gs://test-96f35.appspot.com/images/figure-65.png`,
+    )
+    console.log(result)
+    const fullTextAnnotation = result.fullTextAnnotation
+    console.log(`Full text:`)
+    console.log(`${fullTextAnnotation.text}`)
+    return res.status(200).json({
+      Success: `${fullTextAnnotation.text}
+    `,
+    })
+    //return res.status(200).json({ Success: 'Success' })
+  } catch (e) {
+    console.log('Error', e)
+    return res.status(401).json({
+      Error: JSON.stringify(e, Object.getOwnPropertyNames(e)),
+    })
+  }
+  // checks everything
+  // fullTextAnnotation.pages.forEach((page) => {
+  //   page.blocks.forEach((block) => {
+  //     console.log(`Block confidence: ${block.confidence}`)
+  //     block.paragraphs.forEach((paragraph) => {
+  //       console.log(`Paragraph confidence: ${paragraph.confidence}`)
+  //       paragraph.words.forEach((word) => {
+  //         const wordText = word.symbols.map((s) => s.text).join('')
+  //         console.log(`Word text: ${wordText}`)
+  //         console.log(`Word confidence: ${word.confidence}`)
+  //         word.symbols.forEach((symbol) => {
+  //           console.log(`Symbol text: ${symbol.text}`)
+  //           console.log(`Symbol confidence: ${symbol.confidence}`)
+  //         })
+  //       })
+  //     })
+  //   })
+  // })
+}
+
+module.exports = {
+  scanfile: scanfile,
+}
